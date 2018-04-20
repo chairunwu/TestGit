5c9a0812c50ca2b57ec850ebf8468a85d561c243
chairunwu <516485679@qq.com>
2018-04-20 10:38:09 +0800

    controller提交

---
+++ src/com/test/TestSpringBootController.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class TestSpringBootController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "hahahah";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootController.class, args);
+	}
+}
